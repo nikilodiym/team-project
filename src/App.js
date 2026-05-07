82306d3cc28269533e8c07768a4263855c64c64f
Nikita Serhiyovych Krenyov <134860909+nikilodiym@users.noreply.github.com>
2026-05-07 20:36:06 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
 import Header from "./components/Header";
+import FormEditor from "./screens/components/FormEditor";
 import Home from "./screens/Home";
 import FAQ from "./screens/FAQ/FAQ";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App(){
-    return (
-        <BrowserRouter>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/FAQ" element={<FAQ />} />
-            </Routes>
-        </BrowserRouter>
-    );
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/FAQ" element={<FAQ />} />
+        <Route path="/editor" element={<FormEditor />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
