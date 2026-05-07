6d7961931edd3b0968e5a12e3c322100bd4179d8
Nikita Serhiyovych Krenyov <134860909+nikilodiym@users.noreply.github.com>
2026-05-07 19:12:26 +0300

    commit

---
+++ src/screens/components/NewForm.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./NewForm.css";
+
+export default function NewForm() {
+    return (
+        <section className="new-form">
+            <h2>Нова форма</h2>
+
+            <div className="form-card">
+                <div className="plus">
+                    <div className="vertical"></div>
+                    <div className="horizontal"></div>
+                </div>
+            </div>
+        </section>
+    )
+}
